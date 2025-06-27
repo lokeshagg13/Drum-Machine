97359b0018e7c85cd080877a1761dd89fde5e989
lokeshagg13 <lkaggarwal1997@gmail.com>
2025-06-27 22:46:06 +0530

    [DEPLOY]

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import MainPanel from "./components/mainPanel";
+import MainPanel from "./components/MainPanel";
+import Footer from "./components/ui/elements/Footer";
 import HeaderImage from "./images/header.png";
 
 function App() {
   return (
-    <div className="app p-4">
+    <div className="app p-4 flex flex-col">
       <div className="flex items-center justify-center mb-4">
         <img src={HeaderImage} alt="Drum Machine" width={300} />
       </div>
       <MainPanel />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/ui/elements/Footer.jsx
@@ -0,0 +1,16 @@
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <div className="footer-text">
+          Designed and Developed by
+          <span className="footer-name"> Lokesh Aggarwal</span>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
